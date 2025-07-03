50c52b7faaf32b8805af232580a2fdcf05f8fe8d
Sr-Yagame <ghdyagame74@gmail.com>
2025-07-03 15:14:35 -0300

    Update send.js

--- api/send.js
+++ api/send.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Authorization, Content-Type');
 
   if (req.method !== 'POST') {
