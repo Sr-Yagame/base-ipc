73695a7281f8e741b19de6493d6fc8b7f4787e09
Sr-Yagame <ghdyagame74@gmail.com>
2025-07-17 19:07:40 -0300

    Update send.js

--- api/send.js
+++ api/send.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://redirecting-wait.netlify.app/');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Authorization, Content-Type');
 
